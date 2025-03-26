0f2db8128ebf262bb06bd5a9b82a4a1f15797a7b
alainkatayi <alainkatayi01@gmail.com>
2025-03-26 23:09:26 +0100

    creation de tous les composants necessaires et definition des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './composants/login/login.component';
+import { RegisterComponent } from './composants/register/register.component';
+import { DashboardComponent } from './composants/dashboard/dashboard.component';
+import { CreateComponent } from './composants/create/create.component';
+import { UpdateComponent } from './composants/update/update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:LoginComponent,
+        title:'login'
+    },
+
+    {
+        path:'register',
+        component:RegisterComponent,
+        title:'register'
+    },
+
+    {
+        path:'dashboard',
+        component:DashboardComponent,
+        title:'dashboard'
+    },
+
+    {
+        path:'create',
+        component:CreateComponent,
+        title:'create-task'
+    },
+
+    {
+        path:'update/:id',
+        component:UpdateComponent,
+        title:'update'
+    }
+];
