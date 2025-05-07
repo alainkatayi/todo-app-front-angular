583020abeaa95b88f2051fdcb7ffadd2c9ee5aea
alainkatayi <alainkatayi01@gmail.com>
2025-05-07 15:07:41 +0100

    Implementation du login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Router } from '@angular/router'; 
+import { AuthService } from '../../service/auth.service';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'] 
 })
 export class LoginComponent {
+  service: AuthService = inject(AuthService);
+  route: Router = inject(Router);
+  email: string = '';
+  password: string = '';
+  message: string = '';
 
+  login() {
+    this.service.login(this.email, this.password)
+      .then((response: any) => {
+        if (response.data) {
+          localStorage.setItem('token', response.data.token);
+          localStorage.setItem('nom', response.data.name);
+          localStorage.setItem('email', response.data.email);
+          localStorage.setItem('user_id', String(response.data.id));
+
+          this.message = "Connexion réussie !";
+          this.route.navigate(['/home']); 
+        } else {
+          //console.log(this.message)
+          this.message = "Identifiants invalides.";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        this.message = "Erreur de connexion.";
+      });
+
+      console.log(this.email);
+      //console.log(this.message)
+  }
 }
---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    data:{
+        id: number;
+        name:string;
+        email:string;
+        email_verified_at: string;
+        created_at: string;
+        updated_at:string;
+        token:string;
+    }
+
+}
