c76f92fe0b492071625287eec150ffdc58a9ec59
alainkatayi <alainkatayi01@gmail.com>
2025-06-14 15:45:34 +0100

    Creation du model user, pour definir les donnees a utilisé pour l'authentification

---
+++ src/app/models/auth.ts
@@ -0,0 +1,19 @@
+export interface AuthLoginData{
+    email: string
+    password : string
+}
+
+export interface AuthLoginResponse{
+    id:number
+    name: string
+    email : string
+    email_verified_at : string|null
+    token: string
+}
+
+export interface AuthRegisterData{
+    name:string
+    email:string
+    password:string
+    password_confirmation:string
+}
