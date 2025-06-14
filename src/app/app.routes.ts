6221d79ab0f77ba594a39013b6252da47b613f61
alainkatayi <alainkatayi01@gmail.com>
2025-06-14 15:37:24 +0100

    Modification de la route pour le homeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {
+    path :'',
+    title:'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+   },
+   {
+    path:'login',
+    component: LoginComponent
+   },
+   {
+      path:'register',
+      component: RegisterComponent
+   }
+];
+
+@NgModule({
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
+ })
+ export class AppRoutingModule {}
